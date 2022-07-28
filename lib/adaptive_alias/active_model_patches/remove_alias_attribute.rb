2b06af6de61d8cbe283a11428600e782471249f8
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2022-07-28 19:34:37 +0800

    Fix: alias_attribute in rails 6.1, 7.0

--- lib/adaptive_alias/active_model_patches/remove_alias_attribute.rb
+++ lib/adaptive_alias/active_model_patches/remove_alias_attribute.rb
@@ -2,16 +2,71 @@ require 'active_model'
 
 module ActiveModel::AttributeMethods
   module ClassMethods
-    def alias_attribute(new_name, old_name)
-      self.attribute_aliases = attribute_aliases.merge(new_name.to_s => old_name.to_s)
+    if Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('7.0')
+      def alias_attribute(new_name, old_name)
+        self.attribute_aliases = attribute_aliases.merge(new_name.to_s => old_name.to_s)
+        ActiveSupport::CodeGenerator.batch(generated_attribute_methods, __FILE__, __LINE__) do |code_generator|
+          attribute_method_matchers.each do |matcher|
+            method_name = matcher.method_name(new_name).to_s
+            target_name = matcher.method_name(old_name).to_s
+            parameters = matcher.parameters
+
+            mangled_name = target_name
+            unless NAME_COMPILABLE_REGEXP.match?(target_name)
+              mangled_name = "__temp__#{target_name.unpack1("h*")}"
+            end
+
+            code_generator.define_cached_method(method_name, as: mangled_name, namespace: :alias_attribute) do |batch|
+              body = if CALL_COMPILABLE_REGEXP.match?(target_name)
+                       "self.#{target_name}(#{parameters || ''})"
+                     else
+                       call_args = [":'#{target_name}'"]
+                       call_args << parameters if parameters
+                       "send(#{call_args.join(", ")})"
+                     end
+
+              modifier = matcher.parameters == FORWARD_PARAMETERS ? "ruby2_keywords " : ""
+
+              batch <<
+                "#{modifier}def #{mangled_name}(#{parameters || ''})" <<
+                body <<
+                "end"
+            end
+          end
+        end
+      end
+    elsif Gem::Version.new(ActiveRecord::VERSION::STRING) >= Gem::Version.new('6.1')
+      def alias_attribute(new_name, old_name)
+        self.attribute_aliases = attribute_aliases.merge(new_name.to_s => old_name.to_s)
+        CodeGenerator.batch(generated_attribute_methods, __FILE__, __LINE__) do |owner|
+          attribute_method_matchers.each do |matcher|
+            matcher_new = matcher.method_name(new_name).to_s
+            matcher_old = matcher.method_name(old_name).to_s
+            define_proxy_call false, owner, matcher_new, matcher_old
+          end
+        end
+      end
+    else
+      def alias_attribute(new_name, old_name)
+        self.attribute_aliases = attribute_aliases.merge(new_name.to_s => old_name.to_s)
+        attribute_method_matchers.each do |matcher|
+          matcher_new = matcher.method_name(new_name).to_s
+          matcher_old = matcher.method_name(old_name).to_s
+
+          # define alias attribute method in the same module as other attributes do.
+          # If we defined in self(model class), we will have to undef it in model class, too.
+          # But undef methods in model class, those methods will never be accessible even if we define them in generated_attribute_methods module.
+          define_proxy_call false, generated_attribute_methods, matcher_new, matcher_old
+        end
+      end
+    end
+
+    def remove_alias_attribute(new_name)
+      self.attribute_aliases = attribute_aliases.except(new_name.to_s)
+
       attribute_method_matchers.each do |matcher|
         matcher_new = matcher.method_name(new_name).to_s
-        matcher_old = matcher.method_name(old_name).to_s
-
-        # define alias attribute method in the same module as other attributes do.
-        # If we defined in self(model class), we will have to undef it in model class, too.
-        # But undef methods in model class, those methods will never be accessible even if we define them in generated_attribute_methods module.
-        define_proxy_call false, generated_attribute_methods, matcher_new, matcher_old
+        remove_proxy_call generated_attribute_methods, matcher_new
       end
     end
 
@@ -23,18 +78,6 @@ module ActiveModel::AttributeMethods
              end
 
       mod.module_eval defn, __FILE__, __LINE__ + 1
-    end
-
-    def remove_alias_attribute(new_name)
-      # association_scope -> add_constraints -> last_chain_scope -> where!(key => model[foreign_key])
-      # self[attr_name] -> read_attribute(attr_name) -> attribute_aliases
-      # where! -> where_clause_factory.build -> attributes = predicate_builder.resolve_column_aliases(opts) -> attribute_aliases
-      self.attribute_aliases = attribute_aliases.except(new_name.to_s)
-
-      attribute_method_matchers.each do |matcher|
-        matcher_new = matcher.method_name(new_name).to_s
-        remove_proxy_call generated_attribute_methods, matcher_new
-      end
     end
   end
 end
