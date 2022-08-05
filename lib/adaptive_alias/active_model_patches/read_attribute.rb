7fda56eb5a60156cdb4591da2441d8a80e1a8875
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2022-08-05 19:04:26 +0800

    prevent adding methods directly in class / module

--- lib/adaptive_alias/active_model_patches/read_attribute.rb
+++ lib/adaptive_alias/active_model_patches/read_attribute.rb
@@ -1,22 +1,30 @@
 require 'active_record'
 
+module AdaptiveAlias
+  module ActiveModelPatches
+    module ReadAttribute
+      def read_attribute(attr_name, &block) # :nodoc:
+        name = attr_name.to_s
+        name = self.class.attribute_aliases[name] || name
+
+        name = @primary_key if name == 'id' && @primary_key
+        _read_attribute(name, &block)
+      end
+
+      # This method exists to avoid the expensive primary_key check internally, without
+      # breaking compatibility with the write_attribute API
+      def _read_attribute(attr_name, &block) # :nodoc:
+        name = attr_name.to_s
+        name = self.class.attribute_aliases[name] || name
+
+        sync_with_transaction_state if @transaction_state&.finalized?
+        return yield(name) if block_given? and AdaptiveAlias.missing_value?(@attributes, self.class, name)
+        return @attributes.fetch_value(name, &block)
+      end
+    end
+  end
+end
+
 module ActiveRecord::AttributeMethods::Read
-  def read_attribute(attr_name, &block) # :nodoc:
-    name = attr_name.to_s
-    name = self.class.attribute_aliases[name] || name
-
-    name = @primary_key if name == 'id' && @primary_key
-    _read_attribute(name, &block)
-  end
-
-  # This method exists to avoid the expensive primary_key check internally, without
-  # breaking compatibility with the write_attribute API
-  def _read_attribute(attr_name, &block) # :nodoc:
-    name = attr_name.to_s
-    name = self.class.attribute_aliases[name] || name
-
-    sync_with_transaction_state if @transaction_state&.finalized?
-    return yield(name) if block_given? and AdaptiveAlias.missing_value?(@attributes, self.class, name)
-    return @attributes.fetch_value(name, &block)
-  end
+  prepend AdaptiveAlias::ActiveModelPatches::ReadAttribute
 end
--- lib/adaptive_alias/active_model_patches/write_attribute.rb
+++ lib/adaptive_alias/active_model_patches/write_attribute.rb
@@ -1,22 +1,25 @@
 require 'active_record'
 
+module AdaptiveAlias
+  module ActiveModelPatches
+    module WriteAttribute
+      def write_attribute(attr_name, value)
+        name = attr_name.to_s
+        name = self.class.attribute_aliases[name] || name
+        super(name, value)
+      end
+
+      # This method exists to avoid the expensive primary_key check internally, without
+      # breaking compatibility with the write_attribute API
+      def _write_attribute(attr_name, value) # :nodoc:
+        name = attr_name.to_s
+        name = self.class.attribute_aliases[name] || name
+        super(name, value)
+      end
+    end
+  end
+end
+
 module ActiveRecord::AttributeMethods::Write
-  def write_attribute(attr_name, value)
-    name = attr_name.to_s
-    name = self.class.attribute_aliases[name] || name
-
-    name = @primary_key if name == 'id' && @primary_key
-    _write_attribute(name, value)
-  end
-
-  # This method exists to avoid the expensive primary_key check internally, without
-  # breaking compatibility with the write_attribute API
-  def _write_attribute(attr_name, value) # :nodoc:
-    name = attr_name.to_s
-    name = self.class.attribute_aliases[name] || name
-
-    sync_with_transaction_state if @transaction_state&.finalized?
-    @attributes.write_from_user(name, value)
-    value
-  end
+  prepend AdaptiveAlias::ActiveModelPatches::WriteAttribute
 end
