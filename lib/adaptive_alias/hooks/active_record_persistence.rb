aeadb8088f7c62f083fd0d83a1b2677839410e1d
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2022-10-08 02:35:20 +0800

    Fix: attribute set in _update_record

--- lib/adaptive_alias/hooks/active_record_persistence.rb
+++ lib/adaptive_alias/hooks/active_record_persistence.rb
@@ -1,6 +1,13 @@
 module AdaptiveAlias
   module Hooks
     module ActiveRecordPersistence
+      def _update_record
+        AdaptiveAlias.rescue_statement_invalid(model: self) do
+          attribute_set_fix!
+          super
+        end
+      end
+
       def _create_record(*)
         AdaptiveAlias.rescue_statement_invalid(model: self) do
           attribute_set_fix!
@@ -13,10 +20,27 @@ module AdaptiveAlias
       def attribute_set_fix!
         if attribute_names.any?{|name| self.class.attribute_aliases[name] }
           attributes = @attributes.instance_variable_get(:@attributes)
+          attributes = attributes.instance_variable_get(:@delegate_hash) if attributes.is_a?(ActiveModel::LazyAttributeHash)
+
+          delete_duplicate_keys!(attributes)
+
           attributes.transform_keys! do |key|
             self.class.attribute_aliases[key] || key
           end
         end
+      end
+
+      # delete duplicate keys caused by instantiate after migration but before patch removal.
+      def delete_duplicate_keys!(attributes)
+        duplicate_keys = {}
+        delete_keys = []
+        attributes.each do |key, _|
+          aliased_key = self.class.attribute_aliases[key] || key
+          delete_keys << key if duplicate_keys[aliased_key] # delete old key and keep the new one
+          duplicate_keys[aliased_key] = true
+        end
+
+        delete_keys.each{|s| attributes.delete(s) }
       end
     end
   end
